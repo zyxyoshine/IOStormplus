9e61995d9ee182841bffa0c11c2af79a30d78e97
Yuxing Zhou <t-yuxzho@microsoft.com>
2018-04-26 15:28:14 +0800

    fix bugs

--- deploy/deploy.linux.sh
+++ deploy/deploy.linux.sh
@@ -1,4 +1,3 @@
-sudo -s
 apt-get update
 apt-get upgrade -y
 wget http://brick.kernel.dk/snaps/fio-3.5.tar.bz2
@@ -7,8 +6,8 @@ apt-get install make
 apt-get install gcc -y
 apt-get install g++ -y
 ./fio-3.5/configure
-cd ~/fio-3.5 && make
-cd ~/fio-3.5 && make install
+cd fio-3.5 && make
+cd fio-3.5 && make install
 apt-get install -y samba samba-common python-glade2 system-config-samba
 rm -f /etc/samba/smb.conf
 cd /etc/samba && wget https://raw.githubusercontent.com/zyxyoshine/IOStromplus/master/deploy/smb.conf
@@ -25,6 +24,6 @@ cat /dev/null  > /samba/temp/controller.tmp
 chmod -R 0777 /samba/temp/controller.tmp
 chown -R nobody:nogroup /samba/temp/controller.tmp
 service smbd restart
-mkdir ~/fiojob
-cd ~/fiojob && wget https://raw.githubusercontent.com/zyxyoshine/IOStromplus/master/src/agent.linux.cpp
-g++ -std=c++11 agent.linux.cpp -o agent
+mkdir /home/vmroot/fiojob
+cd /home/vmroot/fiojob && wget https://raw.githubusercontent.com/zyxyoshine/IOStromplus/master/src/agent.linux.cpp
+cd /home/vmroot/fiojob && g++ -std=c++11 agent.linux.cpp -o agent
