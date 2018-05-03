5bee8b3faddda52a1b4366d5a6d5056e3032a86f
Yuxing Zhou <t-yuxzho@microsoft.com>
2018-05-03 17:07:38 +0800

    add deploy template and script for controller

---
+++ deploy/deploy.controller.ps1
@@ -0,0 +1,18 @@
+﻿$Root = "C:\"
+
+#Download and unzip controller package
+
+$PackageName = "Controller.zip"
+$PackageUrl = "https://github.com/zyxyoshine/IOStromplus/raw/master/deploy/binary/Controller.zip"
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+Invoke-WebRequest -Uri $PackageUrl -OutFile ($Root + $PackageName)
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip ($Root + $PackageName) $Root
