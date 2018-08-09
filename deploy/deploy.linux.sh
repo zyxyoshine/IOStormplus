805b047f9e57d81a5865e55112e86a2b9051c98f
zyxyoshine <zyx.pulsars@gmail.com>
2018-08-09 16:35:02 +0800

    update script and template for create one volume striped over all data disks on linux

--- deploy/deploy.linux.sh
+++ deploy/deploy.linux.sh
@@ -10,9 +10,18 @@ ln -s /home/IOStormplus/libcpprest.so.2.9 /usr/lib/libcpprest.so.2.9
 chmod -R 0777 /home/IOStormplus
 cd /home && wget http://brick.kernel.dk/snaps/fio-3.5.tar.bz2
 cd /home && tar -xjvf fio-3.5.tar.bz2
-./fio-3.5/configure
+cd /home && ./fio-3.5/configure
 cd /home/fio-3.5 && make
 cd /home/fio-3.5 && make install
 cat /dev/null > /etc/profile.d/IOStormplus.sh
 echo "nohup /home/IOStormplus/agent $(hostname --ip-address) $2 $3 \"$1\" >/dev/null 2>agent.err &" > /etc/profile.d/IOStormplus.sh
 cd /home/IOStormplus && nohup ./agent $(hostname --ip-address) $2 $3 $1 >/dev/null 2>agent.err &
+#create one volume striped over all data disks
+disks=($(lsblk -l -p -o NAME | grep "sd" | grep -v "sda" | grep -v "sdb"))
+pvcreate ${disks[*]}
+vgcreate datavg ${disks[*]}
+lvcreate -l 100%FREE --type striped -i ${#disks[@]} -n datav datavg
+mkfs -t ext3 /dev/mapper/datavg-datav
+mkdir /mnt/data
+mount -t ext3 /dev/mapper/datavg-datav  /mnt/data
+chmod 777 /mnt/data
