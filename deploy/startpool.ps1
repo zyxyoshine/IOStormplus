ebbec1f322d72cc12e42dfa31c4d90e23bf0524f
Bekim Demiroski <bekimd@microsoft.com>
2018-08-15 16:01:40 +0800

    added async ops for VMs

--- deploy/startpool.ps1
+++ deploy/startpool.ps1
@@ -10,5 +10,40 @@ $dn="iostorm"+$vmPool
 
 #check if the state of the pool machiknes in the pool is not "stopping".  
 $vms = get-azurermvm | where {$_.Tags.pool -eq $vmPool} | where ResourceGroupName -eq $rg
+$context = Get-AzureRMContext;
 
-$vms | Start-AzureRmVM -Verbose
+$script = {
+    Param( $rg, $vmname, $context )
+    Start-AzureRMVM -ResourceGroupName $rg -Name $vmname -AzureRmContext $context
+}
+
+$jobs = @()
+
+$vms | %{ 
+    $job = Start-Job -Name $_.Name $script -ArgumentList $rg, $_.Name, $context 
+    Write-Output "($_.Name) being stopped in background job $($job.Name) ID $($job.Id)"   
+    $jobs += @($job)
+}
+
+
+$sleepTime = 30
+$timeElapsed = 0
+$running = $true
+$timeout = 1200
+
+while($running -and $timeElapsed -le $Timeout)
+{
+	$running = $false
+	Write-Output "Checking job status"
+	$jobs | % {
+		if($_.State -eq 'Running')
+		{
+			$running = $true
+		}
+    }
+    if( $running) {
+	    Start-Sleep -Seconds $sleepTime
+        $timeElapsed += $sleepTime
+    }
+}
+
--- deploy/stoppool.ps1
+++ deploy/stoppool.ps1
@@ -9,5 +9,38 @@ Param(
 
 #check if the state of the pool machiknes in the pool is not "starting". 
 $vms = get-azurermvm | where {$_.Tags.pool -eq $vmPool} | where ResourceGroupName -eq $rg
+$context = Get-AzureRMContext;
 
-$vms | Stop-AzureRmVM -Force
+$jobIds = @()
+
+$script = {
+    Param( $rg, $vmname, $context )
+    Stop-AzureRMVM -ResourceGroupName $rg -Name $vmname -Force -AzureRmContext $context
+}
+
+$vms | %{ 
+    $job = Start-Job -Name $_.Name $script -ArgumentList $rg, $_.Name, $context
+    Write-Output "VM $($_.Name) being stopped in background job $($job.Name) ID $($job.Id)"   
+}
+
+$sleepTime = 30
+$timeElapsed = 0
+$running = $true
+$timeout = 1200
+
+while($running -and $timeElapsed -le $Timeout)
+{
+	$running = $false
+	Write-Output "Checking job status"
+	$jobs | % {
+		if($_.State -eq 'Running')
+		{
+			$running = $true
+		}
+    }
+    if( $running) {
+	    Start-Sleep -Seconds $sleepTime
+        $timeElapsed += $sleepTime
+    }
+}
+
