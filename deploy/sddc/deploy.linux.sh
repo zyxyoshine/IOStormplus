3b7ef9d2a797c326db8d30a008464e45d1798537
zyxyoshine <zyx.pulsars@gmail.com>
2019-01-15 15:24:03 +0800

    add deployment code for SDDC BVT

---
+++ deploy/sddc/deploy.linux.sh
@@ -0,0 +1,30 @@
+﻿apt-get update
+apt-get upgrade -y
+apt-get install make gcc g++ unzip zlib1g-dev libboost-all-dev libssl-dev libxml2-dev libxml++2.6-dev libxml++2.6-doc uuid-dev libaio-dev cmake -y
+cd /home/ && wget https://github.com/zyxyoshine/IOStormplus/raw/master/deploy/binary/Agent.linux.zip
+cd /home/ && unzip Agent.linux.zip
+mkdir /home/IOStormplus/output
+mkdir /home/IOStormplus/workload
+ln -s /home/IOStormplus/libazurestorage.so.3.1 /usr/lib/libazurestorage.so.3
+ln -s /home/IOStormplus/libcpprest.so.2.9 /usr/lib/libcpprest.so.2.9
+chmod -R 0777 /home/IOStormplus
+cd /home && wget http://brick.kernel.dk/snaps/fio-3.5.tar.bz2
+cd /home && tar -xjvf fio-3.5.tar.bz2
+cd /home && ./fio-3.5/configure
+cd /home/fio-3.5 && make
+cd /home/fio-3.5 && make install
+sed -i '$inohup /home/IOStormplus/agent '$(hostname --ip-address)' '$2' '$3' \"'$1'\" >/dev/null 2>agent.err &' /etc/rc.local
+cd /home/IOStormplus && nohup ./agent $(hostname --ip-address) $2 $3 $1 >/dev/null 2>agent.err &
+#create one volume striped over all data disks
+disks=($(lsblk -l -p -o NAME | grep "sd" | grep -v "sda" | grep -v "sdb"))
+for disk in ${disks[*]}
+do
+       echo -e "n\np\n1\n\n\nt\nfd\nw" | fdisk $disk
+done
+pdisks=($(lsblk -l -p -o NAME | grep "sd" | grep -v "sda" | grep -v "sdb" | grep 1))
+mdadm --create /dev/md0 --level 0 --raid-devices ${#pdisks[*]} ${pdisks[*]} --force
+mkfs -t ext4 /dev/md0
+mkdir /data
+chmod -R 777 /data
+echo -e "/dev/md0\t/data\text4\tdefaults\t0\t2" >> /etc/fstab
+mount -a
---
+++ deploy/sddc/deploy.win.ps1
@@ -0,0 +1,94 @@
+﻿$logFile = 'C:\deploylog.txt'
+Start-Transcript $logFile -Append -Force
+
+$Root = "C:\"
+$WorkspacePath = $Root + "IOStormplus\"
+
+#Download and unzip agent package
+
+$PackageName = "Agent.win.zip"
+$PackageUrl = "https://github.com/zyxyoshine/IOStormplus/raw/master/deploy/binary/Agent.win.zip"
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+Invoke-WebRequest -Uri $PackageUrl -OutFile ($Root + $PackageName)
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip ($Root + $PackageName) $Root
+Remove-Item ($Root + $PackageName)
+
+#Install fio
+
+$FioBinaryName = "fio-3.5-x64.msi"
+$DataStamp = get-date -Format yyyyMMdd
+$logFile = '{0}-{1}.log' -f ($WorkspacePath + $FioBinaryName),$DataStamp
+$FioMSIArguments = @(
+    "/i"
+    ('"{0}"' -f ($WorkspacePath + $FioBinaryName))
+    "/qn"
+    "/norestart"
+    "/L*v"
+    $logFile
+)
+Start-Process "msiexec.exe" -ArgumentList $FioMSIArguments -Wait -NoNewWindow
+Remove-Item ($WorkspacePath + $FioBinaryName)
+
+#Initilize data disks
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+
+$letters = 70..89 | ForEach-Object { [char]$_ }
+$count = 0
+$label = "data"
+
+foreach ($disk in $disks) {
+    $driveLetter = $letters[$count].ToString()
+    $disk |
+    Initialize-Disk -PartitionStyle MBR -PassThru |
+    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel ($label + $count) -Confirm:$false -Force -AllocationUnitSize 65536
+    $count++
+}
+
+#Create Azure Storage connection string
+$storageAccountName = 'AccountName=' + $args[0] + ';'
+$storageAccountKey = 'AccountKey=' + $args[1] + ';'
+$storageEndpointSuffix = 'EndpointSuffix=' + $args[2]
+$storageConnectionString = 'DefaultEndpointsProtocol=https;' + $storageAccountName + $storageAccountKey + $storageEndpointSuffix
+
+#Start Agent
+netsh advfirewall set privateprofile state off
+netsh advfirewall set publicprofile state off
+
+$VMSize = $args[3]
+$VMPool = $args[4]
+$VMSize | Out-File ($WorkspacePath + 'vmsize.txt')
+$VMIp = (Get-NetIPAddress -AddressFamily IPv4 | where { $_.InterfaceAlias -notmatch 'Loopback'} | where { $_.InterfaceAlias -notmatch 'vEthernet'}).IPAddress   
+$agentName = "agent.exe"
+$agentPath = $WorkspacePath + $agentName
+$args = ' ' + $VMIp + ' ' + $VMSize + ' ' + $VMPool + ' ' + $storageConnectionString
+$action = New-ScheduledTaskAction -Execute $agentPath -Argument $args -WorkingDirectory $WorkspacePath
+$trigger = @()
+$trigger += New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
+$trigger += New-ScheduledTaskTrigger -AtStartup
+$settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd
+Unregister-ScheduledTask -TaskName "VMIOSTORM" -Confirm:0 -ErrorAction Ignore
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "VMIOSTORM" -Description "VM iostorm agent" -User "System" -RunLevel Highest -Settings $settings
+
+Stop-Transcript
+
+#Enable PSRemoting
+
+$DNSName = $env:COMPUTERNAME 
+Enable-PSRemoting -Force   
+
+New-NetFirewallRule -Name "WinRM HTTPS" -DisplayName "WinRM HTTPS" -Enabled True -Profile "Any" -Action "Allow" -Direction "Inbound" -LocalPort 5986 -Protocol "TCP"    
+
+$thumbprint = (New-SelfSignedCertificate -DnsName $DNSName -CertStoreLocation Cert:\LocalMachine\My).Thumbprint   
+$cmd = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=""$DNSName""; CertificateThumbprint=""$thumbprint""}" 
+
+cmd.exe /C $cmd  
