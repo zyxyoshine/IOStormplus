45d041edb8f5297ecc57986cd7eb9b43b8c90fa2
zyxyoshine <zyx.pulsars@gmail.com>
2019-01-19 20:24:34 +0800

    fix deploy bug

--- deploy/sddc/deploy.linux.sh
+++ deploy/sddc/deploy.linux.sh
@@ -1,5 +1,6 @@
 ﻿echo "test" >> /home/st
-apt-get install make gcc g++ unzip zlib1g-dev libboost-all-dev libssl-dev libxml2-dev libxml++2.6-dev libxml++2.6-doc uuid-dev libaio-dev cmake -y
+sudo -s
+sudo apt-get install make gcc g++ unzip zlib1g-dev libboost-all-dev libssl-dev libxml2-dev libxml++2.6-dev libxml++2.6-doc uuid-dev libaio-dev cmake -y
 cd /home/ && wget https://github.com/zyxyoshine/IOStormplus/raw/master/deploy/binary/Agent.linux.zip
 cd /home/ && unzip Agent.linux.zip
 mkdir /home/IOStormplus/output
